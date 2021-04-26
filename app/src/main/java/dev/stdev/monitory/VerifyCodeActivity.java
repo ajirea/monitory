b328505558f0357c5145e47bd9c5a93d43f80beb
Satria Aji Putra <satmaxt@outlook.com>
2021-04-26 12:42:53 +0700

    Initial commit

---
+++ app/src/main/java/dev/stdev/monitory/VerifyCodeActivity.java
@@ -0,0 +1,21 @@
+package dev.stdev.monitory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class VerifyCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_code);
+    }
+
+    public void actIn(View view) {
+        Intent intent = new Intent(this, PersonalFormActivity.class);
+        startActivity(intent);
+    }
+}
